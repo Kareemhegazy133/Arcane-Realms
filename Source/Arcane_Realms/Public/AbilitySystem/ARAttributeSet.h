61c88d70b8a479813958d7855e2aa49e55627a97
Kareem Hegazy <kthegazy2002@gmail.com>
2023-10-04 20:41:43 -0400

    Init AbilitySystem Component & Attribute Set

---
+++ Source/Arcane_Realms/Public/AbilitySystem/ARAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "ARAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCANE_REALMS_API UARAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
