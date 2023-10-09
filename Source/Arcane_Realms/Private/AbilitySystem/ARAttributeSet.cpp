c9f14fd5f6a205e620987e486d8b7c123f7248aa
Kareem Hegazy <kthegazy2002@gmail.com>
2023-10-09 00:08:09 -0400

    Attribute Accessors

--- Source/Arcane_Realms/Private/AbilitySystem/ARAttributeSet.cpp
+++ Source/Arcane_Realms/Private/AbilitySystem/ARAttributeSet.cpp
@@ -2,12 +2,14 @@
 
 
 #include "AbilitySystem/ARAttributeSet.h"
-#include "AbilitySystemComponent.h"
 #include "Net/UnrealNetwork.h"
 
 UARAttributeSet::UARAttributeSet()
 {
-
+	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(50.f);
+	InitMaxMana(50.f);
 }
 
 void UARAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
